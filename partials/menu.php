78ebb25cd6115104d6516103e4775efab60c0da7
sethficke <sethficke@yahoo.com>
2017-08-15 16:06:50 -0500

    Broke out form into new file, fixed home link

--- partials/menu.php
+++ partials/menu.php
@@ -1,6 +1,6 @@
 <nav id="menu">
     <ul class="links">
-        <li><a href="index.php" alt="Home">Home</a></li>
+        <li><a href="home.php" alt="Home">Home</a></li>
         <li><a href="resume.php" alt="Resume">Resume</a></li>
     </ul>
 </nav>
