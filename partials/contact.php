cda6a677c0d7f6c793ce46c40bffb8a066895ac2
sethficke <sethficke@yahoo.com>
2017-08-16 20:57:59 -0500

    remove contct form stuff

--- partials/contact.php
+++ partials/contact.php
@@ -1,10 +1,23 @@
-<section id="contact">
-    <div class="inner">
-        <section>
-            <?php include "../partials/contact_form.php" ?>
-        </section>
-        <section class="split">
-            <?php include "../partials/contact_info.php" ?>
-        </section>
+<section>
+    <div class="contact-method">
+        <span class="icon alt fa-envelope"></span>
+        <h3>Email</h3>
+        <a href="#">sgficke@gmail.com</a>
+    </div>
+</section>
+<section>
+    <div class="contact-method">
+        <span class="icon alt fa-phone"></span>
+        <h3>Phone</h3>
+        <span>(513) 505-2876</span>
+    </div>
+</section>
+<section>
+    <div class="contact-method">
+        <span class="icon alt fa-home"></span>
+        <h3>Address</h3>
+        <span>2444 Madison Rd #1403<br />
+        Cincinnati, OH 45208<br />
+        United States of America</span>
     </div>
 </section>
