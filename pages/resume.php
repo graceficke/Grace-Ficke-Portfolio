10d562690c1c687181c421267a22a1c4fb15b5ef
Grace <sgficke@gmail.com>
2017-08-16 21:32:29 -0400

    resume

--- pages/resume.php
+++ pages/resume.php
@@ -14,16 +14,9 @@
 			<div id="wrapper">
 
 				<!-- Header -->
-<<<<<<< HEAD
-					<header id="header">
-						<a href="index.html" class="logo"><strong>Grace Ficke</strong> <span>513.505.2876</span></a>
-						<nav>
-							<a href="#menu">Menu</a>
-						</nav>
-					</header>
-=======
+
                 <?php include("../partials/header.php") ?>
->>>>>>> bba3b215857a026ced80ad1da2b962a424f52c87
+
 
 				<!-- Menu -->
                 <?php include("../partials/menu.php") ?>
@@ -36,7 +29,7 @@
 								<img src="images/pic07.jpg" alt="" />
 							</span>
 							<header class="major">
-								<h1>Grace's Resume</h1>
+								<h1>My Resume</h1>
 							</header>
 							<div class="content">
 								<p>sgficke@gmail.com<br />
@@ -48,29 +41,29 @@
 				<!-- Main -->
 					<div id="main">
 
-<<<<<<< HEAD
+
 						<!-- One -->
 							<section id="one">
 								<div class="inner">
 									<header class="major">
-										<h2>Sed amet aliquam</h2>
+										<h2>Work Experence</h2>
 									</header>
-									<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
 								</div>
 							</section>
 
 						<!-- Two -->
 							<section id="two" class="spotlights">
 								<section>
-									<a href="generic.html" class="image">
-										<img src="images/pic08.jpg" alt="" data-position="center center" />
+									<a href="http://www.madisondesign.us/" class="image">
+										<img src="../../images/mdlogo.jpg" alt="" data-position="center center" />
 									</a>
 									<div class="content">
 										<div class="inner">
 											<header class="major">
-												<h3>Orci maecenas</h3>
+												<h3>Madison Design</h3>
 											</header>
-											<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+											<p>Summer 2017 – Covington, Kentucky <br/>
+											Photography and Design intern shot madison’s portfolio headshots, designed employee handbook as well as sketching on projects for their clients, one of which includes the anual smelt fry paper show.</p>
 											<ul class="actions">
 												<li><a href="generic.html" class="button">Learn more</a></li>
 											</ul>
@@ -84,9 +77,11 @@
 									<div class="content">
 										<div class="inner">
 											<header class="major">
-												<h3>Rhoncus magna</h3>
+												<h3>Studio Green</h3>
 											</header>
-											<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+											<p>Summer 2017 – Cincinnati, Ohio
+												Design Intern worked on packaging design, lables, lots of theme boards, prodect concepting and brand stratagy.
+												http://studiogreen.co/</p>
 											<ul class="actions">
 												<li><a href="generic.html" class="button">Learn more</a></li>
 											</ul>
@@ -154,90 +149,22 @@
 									<div class="contact-method">
 										<span class="icon alt fa-envelope"></span>
 										<h3>Email</h3>
-										<a href="#">information@untitled.tld</a>
+										<a href="#">sgficke@gmail.com</a>
 									</div>
 								</section>
 								<section>
 									<div class="contact-method">
 										<span class="icon alt fa-phone"></span>
 										<h3>Phone</h3>
-										<span>(000) 000-0000 x12387</span>
+										<span>(513) 505-2876</span>
 									</div>
 								</section>
 								<section>
 									<div class="contact-method">
 										<span class="icon alt fa-home"></span>
 										<h3>Address</h3>
-										<span>1234 Somewhere Road #5432<br />
-										Nashville, TN 00000<br />
-										United States of America</span>
-									</div>
-								</section>
-							</section>
-						</div>
-					</section>
-=======
-                        <!-- One -->
-                        <section id="one">
-                            <div class="inner">
-                                <header class="major">
-                                    <h1>My Resume</h1>
-                                    <a href="../pdf/Creative%20Circle%20Resume.pdf">Download PDF</a>
-                                </header>
-                                <article>
-                                <!-- Paste your copy here, Grace!  -Seth -->
-                                    
-                                </article>
-                            </div>
-                        </section>
-
-                </div>
->>>>>>> bba3b215857a026ced80ad1da2b962a424f52c87
-
-				<!-- Contact -->
-					<section id="contact">
-						<div class="inner">
-							<section>
-								<form method="post" action="#">
-									<div class="field half first">
-										<label for="name">Name</label>
-										<input type="text" name="name" id="name" />
-									</div>
-									<div class="field half">
-										<label for="email">Email</label>
-										<input type="text" name="email" id="email" />
-									</div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="6"></textarea>
-									</div>
-									<ul class="actions">
-										<li><input type="submit" value="Send Message" class="special" /></li>
-										<li><input type="reset" value="Clear" /></li>
-									</ul>
-								</form>
-							</section>
-							<section class="split">
-								<section>
-									<div class="contact-method">
-										<span class="icon alt fa-envelope"></span>
-										<h3>Email</h3>
-										<a href="#">information@untitled.tld</a>
-									</div>
-								</section>
-								<section>
-									<div class="contact-method">
-										<span class="icon alt fa-phone"></span>
-										<h3>Phone</h3>
-										<span>(000) 000-0000 x12387</span>
-									</div>
-								</section>
-								<section>
-									<div class="contact-method">
-										<span class="icon alt fa-home"></span>
-										<h3>Address</h3>
-										<span>1234 Somewhere Road #5432<br />
-										Nashville, TN 00000<br />
+										<span>2444 Madison Rd. UNIT 1403<br />
+										Cincinnati, OH 45208<br />
 										United States of America</span>
 									</div>
 								</section>
@@ -245,6 +172,7 @@
 						</div>
 					</section>
 
+				
 				<!-- Footer -->
                 <?php include("../partials/footer.php") ?>
 
